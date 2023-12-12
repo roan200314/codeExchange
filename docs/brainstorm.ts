071ffe34bf409ec8a016b3850499dadfd1c8b301
Roan al Helly <roan.al.helly.al.helly@hva.nl>
2023-12-12 19:47:19 +0100

    oop programming

--- docs/brainstorm.ts
+++ docs/brainstorm.ts
@@ -1,109 +1,3 @@
-import "./config";
-import { api, session, url } from "@hboictcloud/api";
-import { User } from "./models/user";
-import { Post } from "./models/post";
-import { runQuery } from "./utils/queryutil";
-
-/**
- * Deze methode wordt aangeroepen als de pagina is geladen, dat gebeurd helemaal onderin!
- */
-async function setup(): Promise<void> {
-    // Kijk of de gebruiker is ingelogd anders mag je hier niet komen.
-    security();
-
-    // Maak een actie aan voor de logout knop. Als je hier op drukt wordt de logout functie aangeroepen
-    document.querySelector(".logout-btn")?.addEventListener("click", logout);
-
-    // Haal alle gegevens van de gebruiker op uit de database en stop dat in het model User
-    const user: User | undefined = await getUserInfo(session.get("user"));
-
-    const post: Post | undefined = await getPostInfo(session.get("post"));
-
-    console.log(post);
-}
-
-/**
- * Check if the user is logged in
- * De methode geeft niets terug (void) en heeft daarom geen return statement
- */
-function security(): void {
-    // Als de sessie met naam user_id niet bestaat (door de ! werkt de if als nietwaar) dan is de gebruiker niet ingelogd
-    if (!session.get("user") || session.get("user") === undefined) {
-        // Stuur de gebruiker door naar de login pagina
-        url.redirect("login.html");
-    }
-}
-
-/**
- * Haal alle gegevens van de gebruiker op uit de database
- * @param id
- * @returns user object
- */
-async function getUserInfo(userid: number): Promise<User | undefined> {
-    try {
-        const data: any = await api.queryDatabase("SELECT * FROM user WHERE id = ?", userid);
-
-        if (data.length > 0) {
-            const user: User = new User(
-                data[0]["id"],
-                data[0]["username"],
-                data[0]["email"],
-                data[0]["firstname"],
-                data[0]["lastname"]
-            );
-            return user;
-        }
-        return undefined;
-    } catch (error) {
-        console.error(error);
-
-        return undefined;
-    }
-}
-
-/**
- * Logout van de gebruiker door de sessie te verwijderen
- * De methode geeft niets terug (void) en heeft daarom geen return statement
- */
-function logout(): void {
-    // Verwijder de sessies
-    const uitloggen: any = confirm("Weet u zeker dat u wilt uitloggen?");
-    if (uitloggen === true) {
-    session.remove("user");
-
-    // Stuur de gebruiker door naar de login pagina
-    url.redirect("login.html");
-    }
-}
-
-/*
-* Haal alle gegevens van de gebruiker op uit de database
-* @param id
-* @returns user object
-*/
-async function getPostInfo(userid: number): Promise<Post | undefined> {
-    try {
-        const data: any = await api.queryDatabase("SELECT * FROM posts");
- 
-        if (data.length > 0) {
-            const post: Post = new Post(
-                data[0]["id"],
-                data[0]["titel"],
-                data[0]["vraag"],
-                data[0]["tijd"],
-                data[0]["tags"],
-                data[0]["code_snippet"]
-            );
-            return post;
-        }
-        return undefined;
-    } catch (error) {
-        console.error(error);
- 
-        return undefined;
-    }
- }
- 
 //////////vragen op scherm laten zien
 // Het element waarin de gegevens worden weergegeven selecteren
 const data: HTMLElement | null = document.getElementById("data");
@@ -184,6 +78,30 @@ async function laatZien(): Promise<void> {
 laatZien();
 
 
+/*
+* Haal alle gegevens van de gebruiker op uit de database
+* @param id
+* @returns user object
+*/
+async function getPostInfo(userid: number): Promise<Post | undefined> {
+   try {
+       const data: any = await api.queryDatabase("SELECT * FROM posts");
 
-// Run bij het opstarten de setup functie
-await setup();
+       if (data.length > 0) {
+           const post: Post = new Post(
+               data[0]["id"],
+               data[0]["titel"],
+               data[0]["vraag"],
+               data[0]["tijd"],
+               data[0]["tags"],
+               data[0]["code_snippet"]
+           );
+           return post;
+       }
+       return undefined;
+   } catch (error) {
+       console.error(error);
+
+       return undefined;
+   }
+}
