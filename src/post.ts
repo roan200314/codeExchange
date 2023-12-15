c0343e2ec4ab34a711fa66381bd4e149a3b93b94
Roan al Helly <roan.al.helly.al.helly@hva.nl>
2023-12-15 16:03:26 +0100

    post aangepast

--- src/post.ts
+++ src/post.ts
@@ -1,42 +1,58 @@
 import "./config";
 import { api, session, url } from "@hboictcloud/api";
 import { User } from "./models/user";
-import { Post } from "./models/post";
 import { runQuery } from "./utils/queryutil";
 
 // Haal het huidige URL op en haal de ID op uit de querystring.
 const currentURL: string = window.location.href;
 const IdOphalen: URL = new URL(currentURL);
-const id: string | null = IdOphalen.searchParams.get("id");
+let id: string | null = IdOphalen.searchParams.get("id");
 
 const posts: any[] | undefined = await runQuery("SELECT * FROM posts WHERE id = (?)", [id]);
-const antwoorden: any[] | undefined = await runQuery("SELECT * FROM answers");
+const antwoorden: any[] | undefined = await runQuery("SELECT * FROM answers WHERE vraag_id = (?)", [id]);
 const users: any[] | undefined = await runQuery("SELECT * FROM user");
+
+const postDB: any = posts[0];
+const antwoordDB: any = antwoorden[0];
 
 
 
 async function setup(): Promise<void> {
+    document.querySelector(".logout-btn")?.addEventListener("click", logout);
 
     // Haal alle gegevens van de gebruiker op uit de database en stop dat in het model User
     const user: User | undefined = await getUserInfo(session.get("user"));
-
-    const post: Post | undefined = await getPostInfo(session.get("post"));
 
     const data: HTMLElement | null = document.getElementById("data");
 
     const div: HTMLElement | null = document.createElement("div");
 
     const titel: HTMLElement | null = document.createElement("p");
-    titel.innerText = post?.titel ?? "Default Value";
+    titel.id = "titel";
+    titel.innerText = `${postDB.titel}`;
     titel.style.color = "black";
+
 
     const vraag: HTMLElement | null = document.createElement("p");
     vraag.id = "vraag";
-    vraag.innerText = post?.vraag ?? "Default Value";
+    vraag.innerText = `vraag: ${postDB.vraag}`;
     vraag.style.color = "black";
+
+    const tags: HTMLElement | null = document.createElement("p");
+    tags.id = "tags";
+    tags.innerText = `tags: ${postDB.tags}`;
+    tags.style.color = "black";
+
+    const antwoord: HTMLElement | null = document.createElement("p");
+    antwoord.id = "antwoord";
+    antwoord.innerText = `antwoord: ${antwoordDB.antwoord}`;
+    antwoord.style.color = "black";
 
     div.appendChild(titel);
     div.appendChild(vraag);
+    div.appendChild(tags);
+    div.appendChild(antwoord);
+
 
     data?.appendChild(div);
 
@@ -48,9 +64,9 @@ async function setup(): Promise<void> {
  * @returns user object
  */
 async function getUserInfo(userid: number): Promise<User | undefined> {
+    console.log(userid);
     try {
         const data: any = await api.queryDatabase("SELECT * FROM user WHERE id = ?", userid);
-
         if (data.length > 0) {
             const user: User = new User(
                 data[0]["id"],
@@ -73,32 +89,16 @@ async function getUserInfo(userid: number): Promise<User | undefined> {
         return undefined;
     }
 }
-/*
-* Haal alle gegevens van de gebruiker op uit de database
-* @param id
-* @returns user object
-*/
-async function getPostInfo(userid: number): Promise<Post | undefined> {
-    try {
-        const data: any = await api.queryDatabase("SELECT * FROM posts WHERE id = ?",[id]);
- 
-        if (data.length > 0) {
-            const post: Post = new Post(
-                data[0]["id"],
-                data[0]["titel"],
-                data[0]["vraag"],
-                data[0]["tijd"],
-                data[0]["tags"],
-                data[0]["code_snippet"]
-            );
-            return post;
-        }
-        return undefined;
-    } catch (error) {
-        console.error(error);
- 
-        return undefined;
+
+function logout(): void {
+    // Verwijder de sessies
+    const uitloggen: any = confirm("Weet u zeker dat u wilt uitloggen?");
+    if (uitloggen === true) {
+    session.remove("user");
+
+    // Stuur de gebruiker door naar de login pagina
+    url.redirect("login.html");
     }
- }
+}
 
  setup();
